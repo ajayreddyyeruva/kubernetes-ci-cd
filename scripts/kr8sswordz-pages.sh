579869ef434202a3cdf0163127677fbba80576d5
Ajay Reddy Yeruva <ajayreddyyeruva@gmail.com>
2018-01-20 14:01:08 -0800

    Update kr8sswordz-pages.sh

--- scripts/kr8sswordz-pages.sh
+++ scripts/kr8sswordz-pages.sh
@@ -12,8 +12,5 @@ sleep 5;
 #Push the images
 docker push mindstreamorg/kr8sswordz:$TAG
 
-#Stop the registry proxy
-docker stop socat-registry
-
 # Create the deployment and service for the front end aka kr8sswordz
 sed 's#mindstreamorg/kr8sswordz:latest#mindstreamorg/kr8sswordz:'$TAG'#' applications/kr8sswordz-pages/k8s/deployment.yaml | kubectl apply -f -
